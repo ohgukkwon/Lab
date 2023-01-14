5bb77bc132f1c9cc3ca08950d12b56e1a372f07c
ohgukkwon <ohguk.kwon@gmail.com>
2023-01-14 15:51:36 -0800

    012423

--- tf/tf_cert/q2.py
+++ tf/tf_cert/q2.py
@@ -8,44 +8,52 @@
 # You do not need them to solve the question.
 # Lambda layers are not supported by the grading infrastructure.
 #
-# You must use the Submit and Test model button to submit your model
+# You must use the Submit and Test button to submit your model
 # at least once in this category before you finally submit your exam,
 # otherwise you will score zero for this category.
 # ======================================================================
 #
-# Getting Started Question
+# Basic Datasets Question
 #
-# Given this data, train a neural network to match the xs to the ys
-# So that a predictor for a new value of X will give a float value
-# very close to the desired answer
-# i.e. print(model.predict([10.0])) would give a satisfactory result
-# The test infrastructure expects a trained model that accepts
-# an input shape of [1]
+# Create and train a classifier for the MNIST dataset.
+# Note that the test will expect it to classify 10 classes and that the
+# input shape should be the native size of the MNIST dataset which is
+# 28x28 monochrome. Do not resize the data. Your input layer should accept
+# (28,28) as the input shape only. If you amend this, the tests will fail.
+#
  
-import numpy as np
 import tensorflow as tf
 from tensorflow import keras
  
  
 def solution_model():
-   xs = np.array([-1.0, 0.0, 1.0, 2.0, 3.0, 4.0], dtype=float)
-   ys = np.array([-7.0, -3.0, 1.0, 5.0, 9.0, 13.0], dtype=float)
+   mnist = tf.keras.datasets.mnist
+   (x_train, y_train),(x_test, y_test) = mnist.load_data()
+   x_train, x_test = x_train / 255.0, x_test / 255.0
+   #data_shape = training_images.shape
+ 
+   model = tf.keras.models.Sequential([
+       tf.keras.layers.Flatten(input_shape = (28,28)),
+       tf.keras.layers.Dense(512, activation = "relu"),
+       tf.keras.layers.Dense(10, activation = "softmax")
+   ])
+   # Compile the model
+   model.compile(optimizer='adam',
+                 loss='sparse_categorical_crossentropy',
+                 metrics=['accuracy'])
+ 
+   model.fit(x_train, y_train, epochs=10)
  
    # YOUR CODE HERE
-   model = tf.keras.Sequential([keras.layers.Dense(units=1, input_shape=[1])])
-   model.compile(optimizer="sgd", loss="mean_squared_error")
-   model.fit(xs, ys, epochs=1000)
- 
    return model
-model = solution_model()
  
-prediction = model.predict([10, 0])[0]
-print(prediction)
+model = solution_model()
  
 # Note that you'll need to save your model as a .h5 like this.
 # When you press the Submit and Test button, your saved .h5 model will
 # be sent to the testing infrastructure for scoring
 # and the score will be returned to you.
+ 
 if __name__ == '__main__':
    model = solution_model()
    model.save("mymodel.h5")
